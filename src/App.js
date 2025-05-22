a5e7790593559d365b778ff6ee487560aca16117
ZachAhmad15 <smshza2@gmail.com>
2025-05-22 16:22:07 -0500

    This is my first commit which is the start of my react website

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+            <header>Zachary Ahmad</header>
+            <h1>welcome to my app</h1>
+            
+
+        </p>
+        <a></a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
